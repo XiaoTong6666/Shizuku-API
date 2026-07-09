ae08ca468d74b37ed2854e334d1b1f2513cc73c4
NkBe <nikobe918@outlook.com>
2026-07-09 17:54:57 +0800

    Update api version

--- manifest.gradle.kts
+++ manifest.gradle.kts
@@ -1,4 +1,4 @@
 extra["api_version_major"] = 13
-extra["api_version_minor"] = 1
-extra["api_version_patch"] = 5
+extra["api_version_minor"] = 6
+extra["api_version_patch"] = 1
 extra["api_version_name"] = "${extra["api_version_major"]}.${extra["api_version_minor"]}.${extra["api_version_patch"]}"
